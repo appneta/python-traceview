8bf6e35989bcda433b436d3a87ee9709943a3553
Spiridon Eliopoulos <seliopou@gmail.com>
2010-07-12 23:13:34 +0000

    [swig/py] Add oboe.Context.log()

--- oboe/oboe.py
+++ oboe/oboe.py
@@ -1,1 +1,23 @@
 from oboe_ext import *
+
+import types
+
+__all__ = ['config', 'Context', 'UdpReporter', 'Event']
+
+config = dict()
+config['tracing_mode'] = 'through'
+config['reporter_host'] = '127.0.0.1'
+
+
+def log(cls, agent, label, **kwargs):
+    evt = Context.createEvent()
+    evt.addInfo('Agent', agent)
+    evt.addInfo('Label', label)
+
+    for k, v in kwargs.items():
+        evt.addInfo(str(k), str(v))
+
+    reporter = UdpReporter(config['reporter_host'])
+    return reporter.sendReport(evt)
+
+setattr(Context, log.__name__, types.MethodType(log, Context))
