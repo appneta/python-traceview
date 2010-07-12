a09c5ba0fedf9e8c74e5336dcd4bf365ff440266
Spiridon Eliopoulos <seliopou@gmail.com>
2010-07-12 22:54:04 +0000

    [swig] Rename swig modue oboe_ext

--- oboe/setup.py
+++ oboe/setup.py
@@ -2,12 +2,12 @@
 
 from distutils.core import setup, Extension
 
-oboe_module = Extension('_oboe', sources=['oboe_wrap.cxx'], libraries=['oboe'])
+oboe_module = Extension('_oboe_ext', sources=['oboe_wrap.cxx'], libraries=['oboe'])
 
 setup(name = 'oboe',
       version = '0.1.0',
       author = 'Spiridon Eliopoulos',
       description = 'Oboe API for Python',
       ext_modules = [oboe_module],
-      py_modules = ['oboe'],
+      py_modules = ['oboe_ext', 'oboe'],
       )
