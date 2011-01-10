3eacb01e56ad45bea4f2b6e32f6a0f4ee2a4cf60
Spiridon Eliopoulos <seliopou@gmail.com>
2011-01-10 23:10:20 +0000

    [swig] Fix memory leak in Context::copy()
    Also bump the python and ruby package numbers.

--- oboe/setup.py
+++ oboe/setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup, Extension
 oboe_module = Extension('_oboe_ext', sources=['oboe_wrap.cxx'], libraries=['oboe'])
 
 setup(name = 'oboe',
-      version = '0.1.0',
+      version = '0.1.2',
       author = 'Spiridon Eliopoulos',
       description = 'Oboe API for Python',
       ext_modules = [oboe_module],
