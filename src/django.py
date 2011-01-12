b88eb3ebded2373175c00f5b0d75ee2065952c59
chris erway <cce@tra.in>
2011-01-12 17:38:38 -0500

    [oboeware] better Django middleware

--- src/django.py
+++ src/django.py
@@ -1,25 +1,49 @@
 # django middleware for passing values to oboe
-
 __all__ = ("OboeDjangoMiddleware", )
 
-# hooks listed here: http://docs.djangoproject.com/en/dev/ref/middleware/
+import oboe
+
+# Middleware hooks listed here: http://docs.djangoproject.com/en/dev/ref/middleware/
 
 class OboeDjangoMiddleware(object):
+
+    def _singleline(s): # some logs like single-line errors better
+        return str(e).replace('\n', ' ').replace('\r', ' ')
     
+    def process_request(self, request):
+        if not oboe.Context.isValid(): return
+        try:
+            evt = oboe.Context.createEvent()
+            evt.addInfo('Agent', 'wsgi')
+            evt.addInfo('Label', 'process_request')
+            evt.addInfo('Framework', 'Django')
+            reporter = oboe.reporter().sendReport(evt)
+        except Exception, e:
+            print >> sys.stderr, "Oboe middleware error:", _singleline(e)
+
     def process_view(self, request, view_func, view_args, view_kwargs):
-        evt = Context.createEvent()
-        evt.addInfo('Agent', 'django')
-        evt.addInfo('Label', 'process_view')
-        evt.addInfo('Controller', view_func.__name__)
-        evt.addInfo('View-args', view_args)
-        evt.addInfo('View-kwargs', view_kwargs)
-        reporter = oboe.reporter().sendReport(evt)
-        #request.environ['wsgiorg.routing_args'] = (view_args, view_kwargs.copy())
-        #request.environ['wsgiorg.routing_args'] = ( view_args, dict(controller=view_func.__name__) )
+        if not oboe.Context.isValid(): return
+        try:
+            evt = oboe.Context.createEvent()
+            evt.addInfo('Agent', 'wsgi')
+            evt.addInfo('Label', 'process_view')
+            evt.addInfo('Framework', 'Django')
+            evt.addInfo('Controller', 'view')
+            evt.addInfo('Action', view_func.__name__)
+            evt.addInfo('View-args', str(view_args))
+            evt.addInfo('View-kwargs', str(view_kwargs))
+            reporter = oboe.reporter().sendReport(evt)
+        except Exception, e:
+            print >> sys.stderr, "Oboe middleware error:", _singleline(e)
 
     def process_exception(self, request, exception):
-        evt = Context.createEvent()
-        evt.addInfo('Agent', 'django')
-        evt.addInfo('Label', 'error')
-        evt.addInfo('Message', str(exception))
-        reporter = oboe.reporter().sendReport(evt)
+        if not oboe.Context.isValid(): return
+        try:
+            evt = oboe.Context.createEvent()
+            evt.addInfo('Agent', 'wsgi')
+            evt.addInfo('Label', 'error')
+            evt.addInfo('Framework', 'Django')
+            evt.addInfo('Message', str(exception))
+            reporter = oboe.reporter().sendReport(evt)
+        except Exception, e:
+            print >> sys.stderr, "Oboe middleware error:", _singleline(e)
