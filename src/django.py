c4b1e2ec56342b002137ce5487e9f9c9876fb24a
chris erway <cce@tra.in>
2011-01-12 04:08:06 -0500

    [oboeware] more detailed controller info for django

--- src/django.py
+++ src/django.py
@@ -2,11 +2,20 @@
 
 __all__ = ("OboeDjangoMiddleware", )
 
-class OboeDjangoMiddlware(object):
+# hooks listed here: http://docs.djangoproject.com/en/dev/ref/middleware/
+
+class OboeDjangoMiddleware(object):
     
     def process_view(self, request, view_func, view_args, view_kwargs):
+        evt = Context.createEvent()
+        evt.addInfo('Agent', 'django')
+        evt.addInfo('Label', 'process_view')
+        evt.addInfo('Controller', view_func.__name__)
+        evt.addInfo('View-args', view_args)
+        evt.addInfo('View-kwargs', view_kwargs)
+        reporter = oboe.reporter().sendReport(evt)
         #request.environ['wsgiorg.routing_args'] = (view_args, view_kwargs.copy())
-        request.environ['wsgiorg.routing_args'] = ( view_args, dict(controller=view_func.__name__) )
+        #request.environ['wsgiorg.routing_args'] = ( view_args, dict(controller=view_func.__name__) )
 
     def process_exception(self, request, exception):
         evt = Context.createEvent()
