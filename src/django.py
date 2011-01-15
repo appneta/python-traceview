1a38f945cca1288f042d95d903a6b56c394b7fd5
chris erway <cce@tra.in>
2011-01-15 03:50:21 -0500

    [oboeware] add profiler, safer imports, use separate agent for django view

--- src/django.py
+++ src/django.py
@@ -1,16 +1,25 @@
 # django middleware for passing values to oboe
 __all__ = ("OboeDjangoMiddleware", "wrap_middleware_classes")
 
-import oboe
+import sys
 
 # Middleware hooks listed here: http://docs.djangoproject.com/en/dev/ref/middleware/
 
 class OboeDjangoMiddleware(object):
 
-    def _singleline(s): # some logs like single-line errors better
+    def __init__(self):
+        try:
+            import oboe
+        except ImportError:
+            print >> sys.stderr, "[oboeware] Can't import oboe, disabling OboeDjangoMiddleware"
+            from django.core.exceptions import MiddlewareNotUsed
+            raise MiddlewareNotUsed
+
+    def _singleline(self, s): # some logs like single-line errors better
         return str(e).replace('\n', ' ').replace('\r', ' ')
     
     def process_request(self, request):
+        import oboe
         if not oboe.Context.isValid(): return
         try:
             evt = oboe.Context.createEvent()
@@ -21,20 +30,35 @@ class OboeDjangoMiddleware(object):
             print >> sys.stderr, "Oboe middleware error:", _singleline(e)
 
     def process_view(self, request, view_func, view_args, view_kwargs):
+        import oboe
         if not oboe.Context.isValid(): return
         try:
             evt = oboe.Context.createEvent()
-            evt.addInfo('Agent', 'django')
-            evt.addInfo('Label', 'process_view')
+            evt.addInfo('Agent', 'django:view')
+            evt.addInfo('Label', 'entry')
             evt.addInfo('Controller', 'view')
             evt.addInfo('Action', view_func.__name__)
             evt.addInfo('View-args', str(view_args))
             evt.addInfo('View-kwargs', str(view_kwargs))
             reporter = oboe.reporter().sendReport(evt)
+            request._oboe_log_view = True
         except Exception, e:
             print >> sys.stderr, "Oboe middleware error:", _singleline(e)
 
+    def process_response(self, request, response):
+        import oboe
+        if not oboe.Context.isValid() or not getattr(request, '_oboe_log_view', False): return response
+        try:
+            evt = oboe.Context.createEvent()
+            evt.addInfo('Agent', 'django:view')
+            evt.addInfo('Label', 'exit')
+            reporter = oboe.reporter().sendReport(evt)
+        except Exception, e:
+            print >> sys.stderr, "Oboe middleware error:", _singleline(e)
+        return response
+
     def process_exception(self, request, exception):
+        import oboe
         if not oboe.Context.isValid(): return
         try:
             evt = oboe.Context.createEvent()
@@ -46,6 +70,7 @@ class OboeDjangoMiddleware(object):
             print >> sys.stderr, "Oboe middleware error:", _singleline(e)
 
 def middleware_hooks(module, objname):
+    import oboe
     try:
         # wrap middleware callables we want to wrap
         cls = getattr(module, objname, None)
@@ -77,4 +102,4 @@ def wrap_middleware_classes(mw_classes):
         imports.whenImported(i[:dot],
                              functools.partial(middleware_hooks, objname=objname))
 
-    return mw_classes
+    return mw_classes + ('oboeware.django.OboeDjangoMiddleware',)
--- src/middleware.py
+++ src/middleware.py
@@ -1,9 +1,9 @@
 # WSGI middleware for Oboe support
 import oboe
-import sys
+import sys, os
 
 class OboeMiddleware:
-    def __init__(self, app, oboe_config, agent="wsgi"):
+    def __init__(self, app, oboe_config, agent="wsgi", profile=False):
         """
         Takes the app that we're wrapping, as well as a dictionary with oboe
         configuration parameters:
@@ -15,6 +15,13 @@ class OboeMiddleware:
         self.wrapped_app = app
         self.oboe_config = oboe_config
         self.agent = agent
+        self.profile = profile
+
+        if self.profile:
+            try:
+                import cStringIO, cProfile, pstats
+            except ImportError:
+                self.profile = False
 
         if self.oboe_config.get('oboe.tracing_mode'):
             oboe.config['tracing_mode'] = self.oboe_config['oboe.tracing_mode']
@@ -52,7 +59,8 @@ class OboeMiddleware:
             add_header = True
         else:
             add_header = False
-        
+
+        response_body = []
         def wrapped_start_response(status, headers, exc_info=None):
             if add_header:
                 headers.append(("X-Trace", endEvt.metadataString()))
@@ -62,26 +70,52 @@ class OboeMiddleware:
                     endEvt.addInfo("Error", repr(exc))
                     endEvt.addInfo("Backtrace", "".join(tb.format_list(tb.extract_tb(trace))))
             start_response(status, headers)
+            if self.profile: return response_body.append
 
+        stats = None
+        result = None
         try:
-            result = self.wrapped_app(environ, wrapped_start_response)
+            if self.profile:
+                def runapp():
+                    appiter = self.wrapped_app(environ, wrapped_start_response)
+                    response_body.extend(appiter)
+                    if hasattr(appiter, 'close'):
+                        appiter.close()
+
+                import cStringIO, cProfile, pstats # XXX test cProfile and pstats exist
+                p = cProfile.Profile()
+                p.runcall(runapp)
+                body = ''.join(response_body)
+                result = [body]
+
+                sio = cStringIO.StringIO()
+                s = pstats.Stats(p, stream=sio)
+                s.sort_stats('cumulative')
+                s.print_stats(15)
+                stats = sio.getvalue()
+                sio.close()
+            else:
+                result = self.wrapped_app(environ, wrapped_start_response)
+
         except Exception, e:
             self.send_end(tracing_mode, endEvt, environ, True, agent=self.agent)
             raise
 
-        self.send_end(tracing_mode, endEvt, environ, agent=self.agent)
+        self.send_end(tracing_mode, endEvt, environ, agent=self.agent, stats=stats)
         oboe.Context.clear()
 
         return result
 
     @classmethod
-    def send_end(cls, tracing_mode, endEvt, environ, threw_error=None, agent="wsgi"):
+    def send_end(cls, tracing_mode, endEvt, environ, threw_error=None, agent="wsgi", stats=None):
         if oboe.Context.isValid() and tracing_mode != 'never' and endEvt:
             evt = endEvt
 
             evt.addEdge(oboe.Context.get())
             evt.addInfo("Agent", agent)
             evt.addInfo("Label", "exit")
+            if stats: 
+                evt.addInfo("Profile", stats)
             if threw_error:
                 import traceback as tb
                 (t, exc, trace) = sys.exc_info()
