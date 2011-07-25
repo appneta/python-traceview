a3f36e507157c6dc82b148e2ef37085b558ab018
James Sedgwick <james@tracelytics.com>
2011-07-25 16:35:27 +0000

    [profiles] bump versions on gem and egg

--- oboe/setup.py
+++ oboe/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, Extension
 oboe_module = Extension('_oboe_ext', sources=['oboe_wrap.cxx'], libraries=['oboe'])
 
 setup(name = 'oboe',
-      version = '0.1.6.1',
+      version = '0.1.6.2',
       author = 'Tracelytics',
       author_email = 'contact@tracelytics.com',
       url = 'http://www.tracelytics.com',
