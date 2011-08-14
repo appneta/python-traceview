f5bc7ca8f2707b90fe8389195d2756b9957ed443
Spiridon Eliopoulos <seliopou@gmail.com>
2011-08-14 00:26:49 +0000

    [oboeware] Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 from setuptools import setup
 
 setup(name = 'oboeware',
-      version = '0.2.1',
+      version = '0.2.3',
       author = 'Tracelytics',
       author_email = 'contact@tracelytics.com',
       url = 'http://www.tracelytics.com',
