ffa5e7bd46aa8c0b3b4748b82017eaa55def8533
chris erway <cce@tracelytics.com>
2011-08-16 17:53:51 +0000

    [scripts] release script that checks if you are on prod branch, adds a git tag, and pushes to pypi

---
+++ release.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# for use only when you're ready to push from prod -> the public pypi
+#
+
+if [ $(git name-rev HEAD 2> /dev/null | awk "{ print \$2 }") != "prod" ]; then
+  echo "You can only push from prod."
+  exit
+fi
+
+#set -e # stop on first non-zero exit code
+#set -x # show commands as they happen
+
+# check package version
+VERSION=$(python setup.py -V)
+
+# tag release (if tag already exists, bails out)
+if ! git tag rel-$VERSION; then
+  echo "Couldn't create tag for ${VERSION}: if it already exists, you need to bump the version."
+  exit
+fi
+git push --tags
+
+# publish package
+python setup.py register -r http://pypi.tracelytics.com sdist upload -r http://pypi.tracelytics.com
