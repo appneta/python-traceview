e6656e42384b3581e22cb3817d1253b35ac7b009
chris erway <cce@tracelytics.com>
2011-09-25 23:49:49 +0000

    [prod] bump version number for another release (protect django middleware)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 from setuptools import setup
 
 setup(name = 'oboeware',
-      version = '0.2.9',
+      version = '0.3.0',
       author = 'Tracelytics',
       author_email = 'contact@tracelytics.com',
       url = 'http://www.tracelytics.com',
