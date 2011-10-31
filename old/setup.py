30e79d5120612ccf8e40bef70ef1dadb0c7fefb0
chris erway <cce@tracelytics.com>
2011-10-31 16:32:26 -0400

    [build] add keywords to setup.py and make oboeware deprecated package

---
+++ old/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2011 by Tracelytics, Inc.
+# All rights reserved.
+
+
+from setuptools import setup, Extension
+version = '0.4.0'
+
+setup(name = 'oboeware',
+      version = version,
+      author = 'Tracelytics',
+      author_email = 'contact@tracelytics.com',
+      url = 'http://www.tracelytics.com',
+      description  = 'This package has been deprecated: only "oboe" is required.',
+      packages = [],
+      license = 'Tracelytics Alpha Agreement',
+      )
