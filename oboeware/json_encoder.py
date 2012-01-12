c49cf34d9f650bf66d71d549e751f30f7eb36155
Dave Costantino <dave@tracelytics.com>
2012-01-12 20:16:17 +0000

    [oboeware] adding inst_pymongo for pymongo instrumentation.

---
+++ oboeware/json_encoder.py
@@ -0,0 +1,16 @@
+"""
+Handles JSON conversion for various object types that may be found in queries
+"""
+import json
+import datetime
+from bson.objectid import ObjectId
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, ObjectId):
+            return str(obj) 
+        elif isinstance(obj, datetime.datetime):
+            return obj.strftime('%Y-%m-%dT%H:%M:%S')
+        else:
+            return simplejson.JSONEncoder.default(self, obj)
+
