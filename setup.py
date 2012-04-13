88937edcfa15a10152dd444c6dc779abcbf44db3
chris erway <cce@tracelytics.com>
2012-04-13 19:08:26 -0400

    bump version, update CHANGES

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 
 
 from setuptools import setup, Extension
-version = '0.4.6'
+version = '0.4.7'
 
 oboe_module = Extension('oboe._oboe_ext',
                         sources=['oboe/oboe_wrap.cxx'],
