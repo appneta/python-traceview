appneta/python-traceview

commit b258b219a9eb9805e20598fbfa6364e5dc6932a4

[wsgi] explicitly grab Controller and Action keys from wsgiorg.routing_args. for tracelytics/tracelons#1318

oboeware/middleware.py
@@ -155,8 +155,9 @@ class OboeMiddleware:
                 endEvt.addInfo("ErrorClass", exc.__class__.__name__)
                 evt.addInfo("Backtrace", "".join(tb.format_list(tb.extract_tb(trace))))
 
-            # gets controller, layer
+            # gets controller, action
             for k, v in  environ.get('wsgiorg.routing_args', [{},{}])[1].items():
-                evt.addInfo(str(k).capitalize(), str(v))
+                if k in set("controller", "action"):
+                    evt.addInfo(str(k).capitalize(), str(v))
 
             reporter = oboe.reporter().sendReport(evt)

commit 382baecba6b895f43eb9ac59cc34568e924468ba

[middleware] fix syntax error introduced in b258b219a9eb9805e20598fbfa6364e5dc6932a4

oboeware/middleware.py
@@ -157,7 +157,7 @@ class OboeMiddleware:
 
             # gets controller, action
             for k, v in  environ.get('wsgiorg.routing_args', [{},{}])[1].items():
-                if k in set("controller", "action"):
+                if k in set(("controller", "action")):
                     evt.addInfo(str(k).capitalize(), str(v))
 
             reporter = oboe.reporter().sendReport(evt)