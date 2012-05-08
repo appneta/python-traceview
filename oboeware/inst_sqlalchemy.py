8cf2d51b153ca0ab9e6f67c1db1362241248022d
TR Jordan <terral.jordan@gmail.com>
2012-05-08 21:40:26 +0000

    [sqlalchemy] Actually wrap do_execute[many] methods properly, using oboe.log_method. [tracelytics/tracelons#1506]

--- oboeware/inst_sqlalchemy.py
+++ oboeware/inst_sqlalchemy.py
@@ -1,54 +1,21 @@
-import sqlalchemy.engine.default
+import oboe
 
-def wrap_default_dialect(module):
-    """Returns a wrapped version of class"""
-
-    class DefaultDialect(module.engine.default.DefaultDialect):
-
-        def do_executemany(self, cursor, statement, parameters, context=None):
-            try:
-                import oboe
-                oboe_enabled = True
-            except ImportError:
-                oboe_enabled = False
-            if oboe_enabled:
-                oboe.Context.log('sqlalchemy', 'entry', backtrace=True, Query=statement.replace('%s', "''"))
-            try:
-                super(DefaultDialect, self).do_executemany(cursor, statement, parameters, context=context)
-            except Exception, e:
-                if oboe_enabled:
-                    oboe.Context.log(None, 'error', ErrorClass=e.__class__.__name__, ErrorMsg=str(e))
-                raise
-            finally:
-                if oboe_enabled:
-                    oboe.Context.log('sqlalchemy', 'exit')
-
-        def do_execute(self, cursor, statement, parameters, context=None):
-            try:
-                import oboe
-                oboe_enabled = True
-            except ImportError:
-                oboe_enabled = False
-            if oboe_enabled:
-                oboe.Context.log('sqlalchemy', 'entry', backtrace=True, Query=statement.replace('%s', "''"))
-            try:
-                super(DefaultDialect, self).do_execute(cursor, statement, parameters, context=context)
-            except Exception, e:
-                if oboe_enabled:
-                    oboe.Context.log(None, 'error', ErrorClass=e.__class__.__name__, ErrorMsg=str(e))
-                raise
-            finally:
-                if oboe_enabled:
-                    oboe.Context.log('sqlalchemy', 'exit')
-
-    return DefaultDialect
+def wrap_execute(func, f_args, f_kwargs, return_val):
+    return { 'Query': f_args[2].replace('%s', "''") }
 
 def wrap(module):
     """ wrap default SQLAlchemy dialect, to catch execute calls to the cursor. """
-    module.engine.default.DefaultDialect = wrap_default_dialect(module)
+    cls = getattr(module, 'DefaultDialect', None)
+    if cls:
+        do_execute = getattr(cls, 'do_execute', None)
+        if do_execute:
+            cls.do_execute = oboe.Context.log_method(layer='sqlalchemy', backtrace=True, callback=wrap_execute)(do_execute.im_func)
+        do_executemany = getattr(cls, 'do_executemany', None)
+        if do_executemany:
+            cls.do_executemany = oboe.Context.log_method(layer='sqlalchemy', backtrace=True, callback=wrap_execute)(do_executemany.im_func)
 
 try:
     import sqlalchemy
-    wrap(sqlalchemy)
+    wrap(sqlalchemy.engine.default)
 except ImportError:
     pass
