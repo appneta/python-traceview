c8a8ad8602aab5cd7fa266cc60aae4d80660e836
dan kuebrich <dan.kuebrich@gmail.com>
2012-05-10 18:24:05 -0400

    [inst_sqlalchemy] add mysql dialect rollback/commit hooks

--- oboeware/inst_sqlalchemy.py
+++ oboeware/inst_sqlalchemy.py
@@ -5,7 +5,8 @@
 """
 import oboe
 
-METHODS = ['do_execute', 'do_executemany', 'do_rollback', 'do_commit']
+DEFAULT_METHODS = ['do_execute', 'do_executemany', 'do_rollback', 'do_commit']
+DIALECT_METHODS = ['do_rollback', 'do_commit']
 
 # Mapping of method names to assumed SQL statements.
 QUERY_MAP = {'do_rollback': 'ROLLBACK',
@@ -19,18 +20,20 @@ def wrap_execute(func, f_args, _f_kwargs, _return_val):
     else:
         return {}
 
-def wrap(module):
+def wrap(module, class_name, methods):
     """ wrap default SQLAlchemy dialect, to catch execute calls to the cursor. """
-    cls = getattr(module, 'DefaultDialect', None)
+    cls = getattr(module, class_name, None)
     decorate = oboe.Context.log_method(layer='sqlalchemy', backtrace=True, callback=wrap_execute)
     if cls:
-        for method_name in METHODS:
+        for method_name in methods:
             method = getattr(cls, method_name, None)
             if method:
                 setattr(cls, method_name, decorate(method.im_func))
 
 try:
-    import sqlalchemy
-    wrap(sqlalchemy.engine.default)
-except ImportError:
+    import sqlalchemy as sa
+    wrap(sa.engine.default, 'DefaultDialect', DEFAULT_METHODS)
+    for (dialect, class_name) in [(sa.dialects.mysql.base, 'MySQLDialect')]:
+        wrap(dialect, class_name, DIALECT_METHODS)
+except ImportError, e:
     pass
