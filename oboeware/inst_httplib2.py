aa6c7836bb22fec76c592d8e71ced30cfacdf954
TR Jordan <terral.jordan@gmail.com>
2012-06-15 19:20:44 +0000

    [urllib3/httplib2] Consolidate urllib3 and httplib3 wrapping logic.

--- oboeware/inst_httplib2.py
+++ oboeware/inst_httplib2.py
@@ -5,47 +5,19 @@ All rights reserved.
 """
 import sys
 import oboe
-from  urlparse import urlparse
+import functools
+from inst_http import wrap_request_before, wrap_request_after
 
 HTTPLIB2_LAYER = 'httplib2'
-
-def wrap_request_before(func, f_args, f_kwargs):
-    if len(f_args) > 5:
-        headers = f_args[4]
-        from_f_args = True
-    else:
-        headers = f_kwargs['headers']
-        from_f_args = False
-    if headers is None:
-        headers = {}
-    if not 'X-Trace' in headers:
-        headers['X-Trace'] = oboe.Context.toString()
-    if from_f_args:
-        f_args = list(f_args)
-        f_args[4] = headers
-    else:
-        f_kwargs['headers'] = headers
-    return f_args, f_kwargs, {}
-
-def wrap_request_after(func, f_args, f_kwargs, res):
-    info = urlparse(f_args[1])
-    path = info.path
-    if path == '':
-        path = '/'
-    if info.query != '':
-        path += '?' + info.query
-
-    return {'IsService': 'yes',
-            'RemoteProtocol': info.scheme if info.scheme != '' else 'http', # XXX Not Python2.4-friendly
-            'RemoteHost': info.netloc,
-            'ServiceArg': path}
 
 try:
     import httplib2
     try:
+        before_cb = functools.partial(wrap_request_before, header_position=4)
+        after_cb = functools.partial(wrap_request_after, uri_position=1)
         wrapper = oboe.Context.log_method(HTTPLIB2_LAYER,
-                                          before_callback=wrap_request_before,
-                                          callback=wrap_request_after)
+                                          before_callback=before_cb,
+                                          callback=after_cb)
         setattr(httplib2.Http, 'request', wrapper(httplib2.Http.request))
     except Exception, e:
         print >> sys.stderr, "Oboe error:", str(e)
