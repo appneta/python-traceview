cf84746b9d709549bf09b3a70aadcb6d66670cfc
Dan Tillberg <tillberg@tracelytics.com>
2012-06-20 16:11:36 -0400

    [memcache] Stop logging errors for older pylibmc versions without cas/gets.

--- test/setup_test_env.sh
+++ test/setup_test_env.sh
@@ -1,5 +1,6 @@
-virtualenv venv-oboeware-test
-source venv-oboeware-test/bin/activate
+mkdir -p ~/venv
+virtualenv ~/venv/oboeware-test
+source ~/venv/oboeware-test/bin/activate
 
 wget ftp://ftp.tummy.com/pub/python-memcached/python-memcached-latest.tar.gz
 tar xzf python-memcached-latest.tar.gz
