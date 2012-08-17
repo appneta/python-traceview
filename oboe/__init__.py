799983177acc50c728f4ce024b0d3d4bda0dae5f
Dave Costantino <dave@tracelytics.com>
2012-08-17 20:12:15 +0000

    [version] bump to 1.1.2

--- oboe/__init__.py
+++ oboe/__init__.py
@@ -17,7 +17,7 @@ from backport import defaultdict
 
 from decorator import decorator
 
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 __all__ = ['config', 'Context', 'UdpReporter', 'Event']
 
 # configuration defaults
