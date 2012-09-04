c654941b509518a02f7af975e9ec68f05a440f58
Dave Costantino <dave@tracelytics.com>
2012-09-04 20:05:12 +0000

    [version] bumping to 1.1.4 and updating change log.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 """
 
 from setuptools import setup, Extension
-version = '1.1.3'
+version = '1.1.4'
 
 oboe_module = Extension('oboe._oboe_ext',
                         sources=['oboe/oboe_wrap.cxx'],
