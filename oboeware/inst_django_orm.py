22ceb64b0f79846839eddf94de1ee0d5191d3b83
TR Jordan <terral.jordan@gmail.com>
2012-10-16 13:20:03 +0000

    [django_orm] Store backtraces for Django ORM calls.

--- oboeware/inst_django_orm.py
+++ oboeware/inst_django_orm.py
@@ -55,7 +55,7 @@ def wrap(module):
         if getattr(cursor_method, '_oboe_wrapped', False):
             return
 
-        oboe_wrapper = oboe.log_method('djangoORM', callback=wrap_execute)
+        oboe_wrapper = oboe.log_method('djangoORM', callback=wrap_execute, store_backtrace=True)
         setattr(CursorOboeWrapper, 'execute', oboe_wrapper(CursorOboeWrapper.execute))
         setattr(CursorOboeWrapper, 'executemany', oboe_wrapper(CursorOboeWrapper.executemany))
 
