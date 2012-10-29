73b422c302327bc821d3871ef34a531391ee8a19
chris erway <cce@tracelytics.com>
2012-10-29 11:59:47 -0400

    [release] bump version to 1.2.2 for #45

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 """
 
 from setuptools import setup, Extension
-version = '1.2.1'
+version = '1.2.2'
 
 oboe_module = Extension('oboe._oboe_ext',
                         sources=['oboe/oboe_wrap.cxx'],
