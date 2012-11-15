6c68a8b1a0be02731b38bec6685e73151a0c2f35
chris erway <cce@tracelytics.com>
2012-11-15 14:54:18 -0500

    [release] version 1.2.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 """
 
 from setuptools import setup, Extension
-version = '1.2.3'
+version = '1.2.4'
 
 oboe_module = Extension('oboe._oboe_ext',
                         sources=['oboe/oboe_wrap.cxx'],
