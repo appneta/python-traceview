86259da372f36f50fc5947dc37c34f60fee0894d
Dan Kuebrich <dan.kuebrich@gmail.com>
2013-02-03 12:57:34 -0500

    [logging] add default handler (#63)
    suppresses init warning, prints error msgs to stderr

--- oboe/__init__.py
+++ oboe/__init__.py
@@ -16,6 +16,7 @@ from backport import defaultdict
 from decorator import decorator
 
 _log = logging.getLogger(__name__)
+_log.addHandler(logging.StreamHandler()) # use sys.stderr; see oboeware #63
 reporter_instance = None
 
 try:
