0d3e80ab150a66babb47869df1e6d61cbb16132a
Dan Kuebrich <dan.kuebrich@gmail.com>
2013-06-22 14:27:01 -0700

    [redis] add test coverage for a number of commands spanning different types

--- test/unit/test_redis.py
+++ test/unit/test_redis.py
@@ -38,11 +38,13 @@ class TestRedis(base.TraceTestCase):
     def assertNoExtraEvents(self):
         self.assertEqual(0, len(self._last_trace.events()))
 
-    def assertRedisTrace(self, op, num_remote_hosts=1, hit=None, key=False):
+    def assertRedisTrace(self, op, num_remote_hosts=1, hit=None, key=None):
         self.assertHasBaseEntryAndExit()
         self.assertHasRedisCall(op, hit=hit, key=key)
         self.assertHasRemoteHost(num=num_remote_hosts)
         self.assertNoExtraEvents()
+
+    ##### GET/SET/DELETE ######################################################
 
     def test_set(self):
         with self.new_trace():
@@ -62,6 +64,125 @@ class TestRedis(base.TraceTestCase):
             ret = self.client.get('test2')
         self.assertRedisTrace(op='GET', hit=False, key='test2')
         self.assertEqual(None, ret)
+
+    def test_mget(self):
+        kvs = {'key1': 'val1', 'key2': 'val2'}
+        self.client.mset(kvs)
+        self.client.delete('missing')
+        with self.new_trace():
+            ret = self.client.mget(kvs.keys(), 'missing')
+        self.assertRedisTrace(op='MGET')
+        self.assertEqual(ret, kvs.values() + [None])
+
+    def test_mset(self):
+        kvs = {'key1': 'val1', 'key2': 'val2'}
+        with self.new_trace():
+            self.client.mset(kvs)
+        self.assertRedisTrace(op='MSET')
+        self.assertEqual(kvs.values(), self.client.mget(kvs.keys()))
+
+    def test_delete(self):
+        self.client.set('test1', 'get_val')
+        self.client.set('test2', 'get_val')
+        with self.new_trace():
+            ret = self.client.delete('test1','test2')
+        self.assertRedisTrace(op='DEL')
+        self.assertEqual(ret, 2)
+
+    ##### TWO-WORD COMMANDS ###################################################
+    # test a few of the two-word commands
+
+    def test_client_list(self):
+        with self.new_trace():
+            ret = self.client.client_list()
+        self.assertRedisTrace(op='CLIENT LIST')
+        self.assertEqual(type(ret), list)
+        self.assertEqual(type(ret[0]), dict)
+
+    def test_script_exists(self):
+        with self.new_trace():
+            ret = self.client.script_exists('xxx')
+        self.assertRedisTrace(op='SCRIPT EXISTS')
+        self.assertEqual(ret, [False])
+
+    ##### LIST COMMANDS #######################################################
+    # test a few of the list commands
+
+    def test_list_lpush(self):
+        self.client.delete('listkey')
+        with self.new_trace():
+            ret = self.client.lpush('listkey','val')
+        self.assertRedisTrace(op='LPUSH', key='listkey')
+        self.assertEqual(ret, 1)
+
+    def test_list_rpop(self):
+        self.client.delete('listkey')
+        ret = self.client.lpush('listkey','val')
+        with self.new_trace():
+            ret = self.client.rpop('listkey')
+        self.assertRedisTrace(op='RPOP', key='listkey')
+        self.assertEqual(ret, 'val')
+
+    ##### HASH COMMANDS #######################################################
+    # test a few of the hash commands
+
+    def test_hset(self):
+        self.client.delete('hashkey')
+        with self.new_trace():
+            ret = self.client.hset('hashkey', 'key', 'val')
+        self.assertRedisTrace(op='HSET', key='hashkey')
+        self.assertEqual(ret, 1)
+
+    def test_hget_hit(self):
+        self.client.delete('hashkey')
+        ret = self.client.hset('hashkey', 'key', 'val')
+        with self.new_trace():
+            ret = self.client.hget('hashkey', 'key')
+        self.assertRedisTrace(op='HGET', hit=True, key='hashkey')
+        self.assertEqual(ret, 'val')
+
+    def test_hget_miss(self):
+        self.client.delete('hashkey')
+        with self.new_trace():
+            ret = self.client.hget('hashkey', 'key')
+        self.assertRedisTrace(op='HGET', hit=False, key='hashkey')
+        self.assertEqual(ret, None)
+
+    ##### SET COMMANDS ########################################################
+    # test a few of the set commands
+
+    def test_set_sadd(self):
+        self.client.delete('setkey')
+        with self.new_trace():
+            ret = self.client.sadd('setkey', 'item')
+        self.assertRedisTrace(op='SADD', key='setkey')
+        self.assertEqual(ret, 1)
+
+    def test_set_srem(self):
+        self.client.delete('setkey')
+        self.client.sadd('setkey', 'item')
+        with self.new_trace():
+            ret = self.client.srem('setkey', 'item')
+        self.assertRedisTrace(op='SREM', key='setkey')
+        self.assertEqual(ret, 1)
+
+    ##### SORTED SET COMMANDS #################################################
+    # test a few of the z-set commands
+
+    def test_set_zadd(self):
+        self.client.delete('setkey')
+        with self.new_trace():
+            ret = self.client.zadd('setkey', 5, 'item')
+        self.assertRedisTrace(op='ZADD', key='setkey')
+        self.assertEqual(ret, 1)
+
+    def test_set_srem(self):
+        self.client.delete('setkey')
+        self.client.zadd('setkey', 5, 'item')
+        with self.new_trace():
+            ret = self.client.zrem('setkey', 'item')
+        self.assertRedisTrace(op='ZREM', key='setkey')
+        self.assertEqual(ret, 1)
 
 if __name__ == '__main__':
     unittest.main()
