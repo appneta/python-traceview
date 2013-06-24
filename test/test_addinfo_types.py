15f008e2fba5a9e175d1f0c99554e30c69d3f45e
Dan Kuebrich <dan.kuebrich@gmail.com>
2013-06-24 15:44:32 -0700

    [test] make test_addinfo_types a TestCase

--- test/test_addinfo_types.py
+++ test/test_addinfo_types.py
@@ -1,16 +1,21 @@
+""" Tests for basic oboe event construction type safety. """
 import oboe
+import unit.base as base
 
-# Event::addInfo(char *,void *)
-# Event::addInfo(char *,std::string const &)
-# Event::addInfo(char *,long)
-# Event::addInfo(char *,double)
+class TestOboeAddInfo(base.TraceTestCase):
 
-evt = oboe.Context.startTrace()
-evt.addInfo('String', 'teststring')
-evt.addInfo('Null', None)
-evt.addInfo('Int', 33)
-evt.addInfo('Float', 33.33)
-evt.addInfo('Array', [33.33, 22])
-evt.addInfo('Map', {'key': 'val'})
+    def __init__(self, *args, **kwargs):
+        super(TestOboeAddInfo, self).__init__(*args, **kwargs)
 
-oboe.reporter().sendReport(evt)
+    def test_addinfo(self):
+        """ Ensures that the c extension doesn't barf on any datatypes a user might log. """
+        md = oboe.Metadata.makeRandom()
+        evt = oboe.SwigEvent.startTrace(md)
+        evt.addInfo('String', 'teststring')
+        evt.addInfo('Null', None)
+        evt.addInfo('Int', 33)
+        evt.addInfo('Float', 33.33)
+        evt.addInfo('Array', [33.33, 22])
+        evt.addInfo('Map', {'key': 'val'})
+        oboe.Context.get_default().report(evt)
+        self.assertTrue(True)
