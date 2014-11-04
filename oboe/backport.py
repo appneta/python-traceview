5852539c729c9bc751a18c7bfa87511730dfa316
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-11-04 22:16:25 +0100

    Py3 future-compatible: use list for dict values

--- oboe/backport.py
+++ oboe/backport.py
@@ -26,7 +26,7 @@ except:
                 args = tuple()
             else:
                 args = self.default_factory,
-            return type(self), args, None, None, self.items()
+            return type(self), args, None, None, list(self.items())
         def copy(self):
             return self.__copy__()
         def __copy__(self):
@@ -34,7 +34,7 @@ except:
         def __deepcopy__(self, memo):
             import copy
             return type(self)(self.default_factory,
-                              copy.deepcopy(self.items()))
+                              copy.deepcopy(list(self.items())))
         def __repr__(self):
             return 'defaultdict(%s, %s)' % (self.default_factory,
                                             dict.__repr__(self))
