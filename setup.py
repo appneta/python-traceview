f7f0063a957cdc27a9bb482e63e4534cd6a9ecc1
Stephen Belanger <sbelanger@appneta.com>
2016-06-28 10:03:16 -0700

    misc: Bump to v2.0.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 import distutils.ccompiler
 from setuptools import setup, Extension
 
-version = '1.5.10'
+version = '2.0.0'
 
 # conditionally build extensions if liboboe and liboboe-dev are available on this platform
 # otherwise, will function in no-op mode: no tracing, but all API endpoints available
